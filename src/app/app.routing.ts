40788bcc921a4c2dfcbc11e1ed938166663e267b
Josué Medino <josuemedino5@gmail.com>
2023-11-02 15:42:25 -0300

    feat: new routers and modify de router-outlet

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,18 @@
 import { Routes, RouterModule, Router } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { OverviewComponent } from './components/overview/overview.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { DataComponent } from './components/data/data.component';
+import { SalesComponent } from './components/sales/sales.component';
 
 
 const routes: Routes = [
-  { path:'**', component: OverviewComponent},
-  { path:'Overview', component:OverviewComponent},
+
   { path:'home', component: HomeComponent},
+  { path:'clients', component: ClientsComponent},
+  { path:'data', component: DataComponent},
+  { path:'sales', component: SalesComponent},
+  { path:'**', component: OverviewComponent},
 
 ];
 
