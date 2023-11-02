55302bce89730d7a04409e97eaacb3525e362f83
Josué Medino <josuemedino5@gmail.com>
2023-11-02 15:43:59 -0300

    feat: created the main layout with sideBar, toolBar and router-outlet

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,33 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import {BreakpointObserver} from '@angular/cdk/layout'
+
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+
+  constructor (private observer:BreakpointObserver){
+
+  }
+
+  ngAfterViewInit(){
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if(res.matches){
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      }else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
+
+}
+
+
